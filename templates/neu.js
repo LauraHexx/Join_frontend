e599c0c5d66fd2b35da8490d40f26754d390809c
LauraHexx <l.hesidenz97@gmail.com>
2023-05-12 16:58:22 +0200

    didididid

--- templates/neu.js
+++ templates/neu.js
@@ -13,3 +13,20 @@ function showEditTask(id, animationClass, className) {
   playAnimation(id, animationClass);
   toggleBlurFilter();
 }
+
+function showErrorsCategoryAlreadyExists() {
+  document.getElementById("errorNameExists").classList.remove("d-none");
+  document.getElementById("errorName").classList.add("d-none");
+}
+
+function deleteAllErrors() {
+  document.getElementById("errorNameExists").classList.add("d-none");
+  document.getElementById("errorName").classList.add("d-none");
+}
+
+/**
+ * This function retrieves the ID of the div element that has the class "selectedColor".
+ *
+ * @returns {string|null} The ID of the selected div element, or null if no element is selected.
+ */
+functio;
