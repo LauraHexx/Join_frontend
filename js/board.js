4892bc27b3b2393458b471fc22550efa388c26d2
LauraHexx <l.hesidenz97@gmail.com>
2023-05-13 19:26:55 +0200

    board - render Tasks start

--- js/board.js
+++ js/board.js
@@ -11,17 +11,22 @@ async function initBoard() {
     "../assets/img/boardDeleteTaskBrightBlue.svg",
     "../assets/img/boardDeleteTaskDarkBlue.svg"
   );
+  renderTasks();
 }
 
 function renderTasks() {
   TASKS.forEach((task) => {
-    const processStepp = task.processStep;
+    const indexOfTask = TASKS.indexOf(task);
     const category = task.category;
     const colorCategory = getColorCategory(category);
-    const title = task.title;
-    const description = task.description;
+    const processStep = task.processStep;
     const contacts = task.contacts;
-    document.getElementById(processStepp).innerHTML += renderTasksHtml(task);
+    document.getElementById(processStep).innerHTML += renderTasksHtml(
+      indexOfTask,
+      task,
+      colorCategory
+    );
+    renderContactsInTasks(indexOfTask, contacts);
   });
 }
 
@@ -31,19 +36,17 @@ function renderTasks() {
  * @param {string} name - The name to look up.
  * @returns {string} The color associated with the given name.
  */
-function getColorByName(name) {
+function getColorCategory(name) {
   const category = CATEGORYS.find((category) => category.name === name);
   return category.color;
 }
 
-function renderTasksHtml(task) {
+function renderTasksHtml(indexOfTask, task, colorCategory) {
   return /*html*/ `
-    <div id="1" class="singleCard" onclick="toggleClass('body', 'overflowHidden'); showEditTask('containerEdit','animation-slideInRight','d-none')" draggable="true" ondragstart="startDragging(1)">
-      <div id="category">${task.category}</div>
+    <div id="1" class="singleCard" ondrop="moveTo('toDo')" ondragover="allowDrop(event) onclick="toggleClass('body', 'overflowHidden'); showEditTask('containerEdit','animation-slideInRight','d-none')" draggable="true" ondragstart="startDragging(1)">
+      <div class="category" id=${colorCategory}>${task.category}</div>
       <div id="title">${task.title}</div>
-      <span id="description">
-      ${task.title}
-      </span>
+      <span id="description">${task.description}</span>
       <div id="progressContainer">
         <div id="progress">
           <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
@@ -51,7 +54,7 @@ function renderTasksHtml(task) {
         <span id="progressAmount">1/2 Done</span>
       </div>
       <div id="contactsAndPrio">
-        <div id="assignedContacts">
+        <div class="assignedContacts" id="${indexOfTask}">
           <div class="initialsOfNames smallCircle">BB</div>
           <div class="initialsOfNames smallCircle">BB</div>
           <div class="initialsOfNames smallCircle">+2</div>
@@ -60,6 +63,12 @@ function renderTasksHtml(task) {
       </div>
     </div>
   `;
+}
+
+function renderContactsInTasks(indexOfTask, contacts) {
+  let amountContacts = contacts.length;
+  console.log(amountContacts);
+  document.getElementById(`assignedContacts${indexOfTask}`);
 }
 
 function startDragging(id) {
