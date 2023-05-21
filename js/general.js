8a595cc8bcef61ea752cd654d427769e9f8dea19
LauraHexx <l.hesidenz97@gmail.com>
2023-05-21 12:01:44 +0200

    refactoren

--- js/general.js
+++ js/general.js
@@ -1,4 +1,6 @@
 let USERS = [];
+let CATEGORYS = [];
+let LOGGED_USER = "";
 
 const SAMPLE_DATA_CONTACTS = [
   {
@@ -64,19 +66,11 @@ const SAMPLE_DATA_CATEGORYS = [
   },
 ];
 
-let CATEGORYS = [];
-
-let LOGGED_USER = "";
-
 async function init(currentHtmlTemplate) {
   await includeHTML();
   styleCurrentSectionInNav(currentHtmlTemplate);
 }
 
-/**
- * Loads HTML Code into elements signed with [w3-include-html]
- * @async
- */
 async function includeHTML() {
   let includeElements = document.querySelectorAll("[w3-include-html]");
   for (let i = 0; i < includeElements.length; i++) {
@@ -84,12 +78,6 @@ async function includeHTML() {
   }
 }
 
-/**
- * Adds HTML code from a file to the given element.
- * @async
- * @param {object} _element The HTML element to which code should be added.
- * @param {string} _file The source file name.
- */
 async function loadHTML(_element, _file) {
   let file = _file;
   let element = _element;
@@ -108,11 +96,6 @@ async function loadHTML(_element, _file) {
   }
 }
 
-/**
- * It is used to check which html template is opened based on the url.
- * It then styles the opened section in the navbar.
- * @param {string} currentHtmlTemplate - Name of html template.
- */
 function styleCurrentSectionInNav(currentHtmlTemplate) {
   if (!currentHtmlTemplate) return;
   const path = window.location.pathname;
@@ -121,12 +104,6 @@ function styleCurrentSectionInNav(currentHtmlTemplate) {
   }
 }
 
-/**
- * Loads users from storage and parses the JSON data.
- * @async
- * @throws {Error} If an error occurs during loading or parsing of user data.
- * @returns {Promise<void>} A promise that resolves when the users are loaded and parsed successfully.
- */
 async function loadUserData() {
   try {
     USERS = JSON.parse(await getItem("users"));
@@ -136,61 +113,15 @@ async function loadUserData() {
   }
 }
 
-/**
- * Loads tasks from storage and parses the JSON data.
- * @async
- * @throws {Error} If an error occurs during loading or parsing of user data.
- * @returns {Promise<void>} A promise that resolves when the users are loaded and parsed successfully.
- */
-async function loadTasks() {
-  try {
-    TASKS = JSON.parse(await getItem("tasks"));
-    console.log("TASKS SERVER", TASKS);
-  } catch (e) {
-    console.error("Loading error:", e);
-  }
-}
-
-/**
- * Loads tasks from storage and parses the JSON data.
- * @async
- * @throws {Error} If an error occurs during loading or parsing of user data.
- * @returns {Promise<void>} A promise that resolves when the users are loaded and parsed successfully.
- */
-async function loadCategorys() {
-  try {
-    CATEGORYS = JSON.parse(await getItem("categorys"));
-    console.log("CATEGORYS SERVER", CATEGORYS);
-  } catch (e) {
-    console.error("Loading error:", e);
-  }
-}
-
-/**
- * Stores the id of the user in the local storage for greeting in summary.
- *  @param {number} userId - The id of the user who will be greeted.
- */
 function setDataForGreeting(userId) {
   setItemInLocalStorage("loggedUserId", userId);
 }
 
-/**
- * Toggles the specified class of the specified element ID. If the element has the
- * class, it will be removed. If it does not have the class, it will be added.
- * @param {string} elementId - The ID of the element to toggle the class for
- * @param {string} className - The class to toggle
- */
 function toggleClass(elementId, className) {
   let element = document.getElementById(elementId);
   element.classList.toggle(className);
 }
 
-/**
- * This function toggles the "blur" filter on certain elements.
- * The elements to be filtered are selected using their CSS selectors.
- * Here, "nav", "header", and "#content" are selected.
- * If the element already has the "filterBlur" class, it is removed. Otherwise, the class is added
- */
 function toggleBlurFilter() {
   const elements = document.querySelectorAll("nav, header, #content");
   elements.forEach((element) => {
@@ -202,22 +133,11 @@ function toggleBlurFilter() {
   });
 }
 
-/**
- * Plays a CSS animation on an element with the given ID.
- * @param {string} id - The ID of the element on which the animation will be played.
- * @param {string} animationClass - The CSS class of the animation to apply to the element.
- */
 function playAnimation(id, animationClass) {
   let element = document.getElementById(id);
   element.classList.add(animationClass);
 }
 
-/**
- * Changes the image displayed when hovering over a specific HTML element or its parent div.
- * @param {string} elementId - The ID of the image element to apply the function to.
- * @param {string} imgSrcOnHover - The file path of the image to display when hovering over the element or its parent div.
- * @param {string} imgSrcOnMouseOut - The file path of the image to display when the mouse leaves the element or its parent div.
- */
 function changeImageOnHover(elementId, imgSrcOnHover, imgSrcOnMouseOut) {
   const element = document.getElementById(elementId);
   const parent = element.parentElement;
@@ -235,39 +155,18 @@ function changeImageOnHover(elementId, imgSrcOnHover, imgSrcOnMouseOut) {
   });
 }
 
-/**
- * Loads a template by updating the window location.
- * @param {string} template - The URL of the template to be loaded.
- * @returns {void}
- */
 function loadTemplate(template) {
   window.location = template;
 }
 
-/**
- * Sets an item in the local storage.
- * @param {string} key - The key under which the item will be stored.
- * @param {any} value - The value to be stored.
- * Note: The value will be converted to a JSON string before storing it in the local storage.
- */
 function setItemInLocalStorage(key, value) {
   localStorage.setItem(key, JSON.stringify(value));
 }
 
-/**
- * Retrieves an item from the local storage.
- * @param {string} key - The key of the item to retrieve.
- * @returns {any} The retrieved value.
- * Note: The value will be parsed from JSON before being returned.
- */
 function getItemFromLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 
-/**
- * Removes an item from the local storage.
- * @param {string} key - The key of the item to remove.
- */
 function removeItemFromLocalStorage(key) {
   localStorage.removeItem(key);
 }
@@ -285,12 +184,6 @@ function hideDisplay(id, className) {
   toggleBlurFilter();
 }
 
-/**
- * Toggles the specified class of the given element ID. If the element has the class, it is removed.
- * If it doesn't have the class, it is added.
- * @param {string} elementId - The ID of the element to toggle the class for.
- * @param {string} className - The class to toggle.
- */
 function toggleClass(elementId, className) {
   let element = document.getElementById(elementId);
   element.classList.toggle(className);
@@ -332,18 +225,9 @@ function hideError(id) {
   }
 }
 
-/**
- * Checks if a value is undefined and removes the "d-none" class from an element with the ID "error" + the variable name if it is.
- *
- * @param {string} id - The name of the variable to check.
- */
 function showError(id) {
   const errorElement = document.getElementById(id);
   errorElement.classList.remove("d-none");
-}
-
-async function sortCategorysAlphabetically() {
-  CATEGORYS = CATEGORYS.sort((a, b) => a.name.localeCompare(b.name));
 }
 
 function getUserId() {
@@ -351,10 +235,6 @@ function getUserId() {
   return id;
 }
 
-/**
- * Generates a random hex code.
- * @returns {string} A random hex code.
- */
 function getRandomColor() {
   return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
