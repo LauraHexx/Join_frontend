2e2923f596d464bebf64c0f1022bccdaa42fad4d
LauraHexx <l.hesidenz97@gmail.com>
2023-06-09 14:53:49 +0200

    test

--- js/header.js
+++ js/header.js
@@ -22,3 +22,13 @@ function toggleMenu() {
   toggleClass("menuHeader", "d-none");
   playAnimation("menuHeader", "animation-menuSlideInRight");
 }
+}
+function toggleMenu() {
+  toggleClass("menuHeader", "d-none");
+  playAnimation("menuHeader", "animation-menuSlideInRight");
+}
+function toggleMenu() {
+  toggleClass("menuHeader", "d-none");
+  playAnimation("menuHeader", "animation-menuSlideInRight");
+}
+
