50e0347adb5602661f9cf93ec1cd02a3226f7e3f
LauraHexx <l.hesidenz97@gmail.com>
2023-06-10 17:46:35 +0200

    test

--- js/header.js
+++ js/header.js
@@ -1,4 +1,8 @@
 function toggleMenu() {
   toggleClass("menuHeader", "d-none");
   playAnimation("menuHeader", "animation-menuSlideInRight");
+}function toggleMenu() {
+  toggleClass("menuHeader", "d-none");
+  playAnimation("menuHeader", "animation-menuSlideInRight");
 }
+
