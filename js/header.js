52af5603ca59b59868bbaf758d1adf77c6a52063
LauraHexx <l.hesidenz97@gmail.com>
2023-06-21 07:56:42 +0200

    menu bg

--- js/header.js
+++ js/header.js
@@ -1,4 +1,10 @@
-function toggleMenu() {
+function openMenu() {
+  toggleClass("menuHeaderBg", "d-none");
   toggleClass("menuHeader", "d-none");
   playAnimation("menuHeader", "animation-menuSlideInRight");
+}
+
+function closeMenu() {
+  toggleClass("menuHeaderBg", "d-none");
+  toggleClass("menuHeader", "d-none");
 }
