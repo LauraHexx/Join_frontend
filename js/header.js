29051b87f4b9329cc6539755378dc2268f3bb5ee
LauraHexx <l.hesidenz97@gmail.com>
2023-06-29 21:14:17 +0200

    test

--- js/header.js
+++ js/header.js
@@ -67,7 +67,3 @@ function closeMenu() {
   toggleClass("menuHeaderBg", "d-none");
   toggleClass("menuHeader", "d-none");
 }
-function closeMenu() {
-  toggleClass("menuHeaderBg", "d-none");
-  toggleClass("menuHeader", "d-none");
-}
