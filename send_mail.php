7b2df8e945ea83c9c63a07ef38b57467542d98ce
LauraHexx <l.hesidenz97@gmail.com>
2023-07-02 13:34:06 +0200

    send email . forgot password

--- send_mail.php
+++ send_mail.php
@@ -2,21 +2,21 @@
 
 ########### CONFIG ###############
 
-$recipient = 'your@mail.com';
-$redirect = 'success.html';
+$redirect = 'http://laura-hesidenz.developerakademie.net/Sakura%20-%20Resp%20Design/index.html';
 
 ########### CONFIG END ###########
 
 
 
-########### Intruction ###########   
+########### Instruction ###########   
 #
 #   This script has been created to send an email to the $recipient
 #   
 #  1) Upload this file to your FTP Server
-#  2) Send a POST rewquest to this file, including
+#  2) Send a POST request to this file, including
 #     [name] The name of the sender (Absender)
-#     [message] Message that should be send to you
+#     [message] Message that should be sent to you
+#     [recipient] The email address where the email should be sent
 #
 ##################################
 
@@ -39,14 +39,17 @@ switch ($_SERVER['REQUEST_METHOD']) {
     case ("POST"): //Send the email;
         header("Access-Control-Allow-Origin: *");
 
-        $subject = "Contact From " . $_POST['name'];
-        $headers = "From:  noreply@developerakademie.com";
+        $recipient = $_POST['recipient'];
+        $subject = "Join - Password Reset Request";
+        $headers = "From: noreply@developerakademie.com";
 
-        mail($recipient, $subject, $_POST['message'], $headers);
+        $message = "Dear Join user\n\nWe have received a request to reset your password for your Join account.\n\nTo proceed with resetting your password, please click on the following link:\n\nhttp://laura-hesidenz.developerakademie.net/templates/resetPassword.html\n\nIf you did not request a password reset, please ignore this email. No changes have been made.\n\nHave a great day :)";
+
+        mail($recipient, $subject, $message, $headers);
         header("Location: " . $redirect); 
 
         break;
-    default: //Reject any non POST or OPTIONS requests.
+        default: //Reject any non POST or OPTIONS requests.
         header("Allow: POST", true, 405);
         exit;
 }
